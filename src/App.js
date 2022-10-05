548106414a86101ea03b0934c51133be4b11e97f
Aditya Soni <54634472+adityasoni2019@users.noreply.github.com>
2022-10-05 16:21:36 +0530

    card list components added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
-
-import logo from './logo.svg';
 import './App.css';
 // this I think is react. 
+
+import {CardList} from './components/card-list/card-list.component';
 
 class App extends Component{
 
@@ -10,29 +10,40 @@ class App extends Component{
     super();
 
     this.state = {
-      monsters: [
-        {
-          name: 'Naman'
-        },
-
-        {
-          name: 'Rohin'
-        },
-        
-        {
-          name: 'Abhishek'
-        }
-      ]
+      monsters: []
     };
-    
+  }
+
+  componentDidMount(){
+      fetch('https://jsonplaceholder.typicode.com/users')
+      .then(r => r.json())
+      .then(user => this.setState({monsters: user}));
   }
 
   render(){
     return (
       <div className="App">
-        {
-          this.state.monsters.map(monster => <h1> {monster.name }</h1>)
+
+      {/* Cardlist is basically a function */}
+      {/* The below code is similar to calling a function. */}
+      {/* We're essentially calling a function. Analogy ofc */}
+      <CardList name_prop = "This is a prop">
+		
+    {/* these 3 H1s are props' children */}
+    {/* props.name_prop = this is a prop */}
+    {/* props.children = array(34, 35, 36)  */}
+    {/* props.children = + all the stuff from line 39*/}
+
+    <h1> Yihua's CardList </h1>
+    <h1> Yihua's CardList </h1>
+    <h1> Yihua's CardList </h1>
+    
+    {
+          this.state.monsters.map(monster => <h1 key = {monster.self_defined_id}> {monster.name}</h1>)
         }
+</CardList>
+
+        {/* And this "function" will basically return whatever's there in the definition of it. */}
       </div>
     )
   }
---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './card-list.styles.css';
+
+export const CardList = function(props){
+
+    // console.log("Hello");
+    // console.log(props);    
+    return <div className='Component Class'>{props.children}</div>;
+}
